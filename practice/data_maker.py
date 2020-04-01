895f62e7c078a7d399a0aa1cd02f6239501d86c2
mattcraigie <matthewcraigie1@gmail.com>
2020-04-01 11:48:46 +1000

    started model fitter

--- practice/data_maker.py
+++ practice/data_maker.py
@@ -2,7 +2,8 @@
 # get output by calling get_data() which gives np array with a[0] as z and a[1] as mags
 
 import numpy as np
-
+from scipy.stats import skewnorm
+import matplotlib.pyplot as plt
 
 def get_data(model, cosmoparams):
     n = 100
@@ -11,7 +12,7 @@ def get_data(model, cosmoparams):
     z = np.random.random_sample([n]) * 0.9 + 0.1
     dist_mod = np.array([model(i, cosmoparams) for i in z])
     # adding noise
-    dist_mod += noise_normal(n)
+    dist_mod += noise_skew(n)
 
     data = np.array([z, dist_mod])
 
@@ -21,7 +22,7 @@ def get_data(model, cosmoparams):
 
     return data, sigma_data
 
-# Types of uncertainty on the magntiudes
+# Types of uncertainty on the magnitudes
 
 
 def noise_normal(n):
@@ -32,14 +33,21 @@ def noise_normal(n):
 
 
 def noise_skew(n):
-    return None
+    centre = 0.0
+    sigma = 0.1
+    a = -1
+    noise = skewnorm.rvs(a, size=n*1000, loc=0, scale=1)
+    plt.hist(noise, bins=30)
+    plt.show()
+    return noise
 
 
 def distance_from_dist_mod(dist_mod):
     luminosity_distance = 10 * 10**(dist_mod/5)
     return luminosity_distance
 
-def app_mag_from_dist_mod:
-    M_B = -19
-    app_mag = mu - M_B
-    return app_mag
+
+def app_mag_from_dist_mod(dist_mod):
+    abs_mag_b = -19
+    app_mag_b = dist_mod - abs_mag_b
+    return app_mag_b
